a426dc385157672ce364772227808b1850030569
Vitor Hugo Homem Marzarotto <59698484+vits-hugs@users.noreply.github.com>
2020-09-22 20:01:15 -0300

    Update and rename bananão.cs to Goblin.cs

---
+++ Jogo de turno/script inimigos/Goblin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goblin : Inimigo
+{
+    
+    public override string Agir()
+    {
+   
+        Atacar(1);
+        Atacar(1);
+        return "hehehehe";
+
+    }
+  
+  
+}
