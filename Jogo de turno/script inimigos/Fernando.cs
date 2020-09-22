b70a6833eb3674c67cbce7c2b9e82a40e645b5e5
Vitor Hugo Homem Marzarotto <59698484+vits-hugs@users.noreply.github.com>
2020-09-22 19:59:08 -0300

    Update Fernando.cs

--- Jogo de turno/script inimigos/Fernando.cs
+++ Jogo de turno/script inimigos/Fernando.cs
@@ -2,20 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fernando : Inimigo
+public class Guarda : Inimigo
 {
-    private string fernando = "Ovo chamar o leonir";
-    private string pastel = "não aguento mais segurar parede";
+    private string aviso = "Toma cuidado";
+    private string soltar = "Eu avisei!!!";
     public override string Agir()
     {
         if(Getvida() == 1)
         {
             SoltarParede();
-            return pastel;
+            return soltar;
         }
-        Debug.Log("paredepareduda");
-        return fernando;
+        return aviso;
     }
+    
     #region ataques
     private void  SoltarParede()
     {
