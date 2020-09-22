7ab367530d5ff53f85139badf473e86847f37ed1
Vitor Hugo Homem Marzarotto <59698484+vits-hugs@users.noreply.github.com>
2020-09-22 20:55:45 -0300

    Update Booom.cs

--- Shooter/Geral/Booom.cs
+++ Shooter/Geral/Booom.cs
@@ -4,21 +4,8 @@ using UnityEngine;
 
 public class Booom : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     void OnCollisionEnter2D (Collision2D col){
         Destroy(col.gameObject);
         Destroy(this.gameObject,0.1f);
-
-
     }
 }
