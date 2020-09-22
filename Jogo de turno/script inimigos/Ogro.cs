7357f45c50ffb06a8a2d258af315c6230d1b7ab0
Vitor Hugo Homem Marzarotto <59698484+vits-hugs@users.noreply.github.com>
2020-09-22 19:57:15 -0300

    Update and rename GordodaBad.cs to Ogro.cs

--- Jogo de turno/script inimigos/Ogro.cs
+++ Jogo de turno/script inimigos/Ogro.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GordodaBad : Inimigo
+public class Ogro : Inimigo
 {
  private string gfala = "BARRIGADA MONSTRO";
  public override string Agir(){
